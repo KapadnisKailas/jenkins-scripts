548c3dbcefa3093f397bfec99610a478a1432368
Kapadnis, Kailas <kapadnis.kailas@gmail.com>
2020-01-26 17:31:16 -0800

    changed double qoutes to single qoutes in job name

--- SpringBootDemoProjectViaDsl.groovy
+++ SpringBootDemoProjectViaDsl.groovy
@@ -1,4 +1,4 @@
-job(“spring-boot-demo-project-via-dsl”){
+job('spring-boot-demo-project-via-dsl'){
   description('maven project job created via dsl')
   scm{
     git('https://github.com/KapadnisKailas/spring-boot-demo-project.git', master)		
