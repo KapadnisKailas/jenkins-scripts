588675a00dad8e77c3e2dd74a28cc3bdb74a6da3
Kapadnis, Kailas <kapadnis.kailas@gmail.com>
2020-01-26 17:33:27 -0800

    taken branch name in single qoutes

--- SpringBootDemoProjectViaDsl.groovy
+++ SpringBootDemoProjectViaDsl.groovy
@@ -1,7 +1,7 @@
 job('spring-boot-demo-project-via-dsl'){
   description('maven project job created via dsl')
   scm{
-    git('https://github.com/KapadnisKailas/spring-boot-demo-project.git', master)		
+    git('https://github.com/KapadnisKailas/spring-boot-demo-project.git', 'master')		
   }
   triggers{
     scm('H/45 * * * *')
