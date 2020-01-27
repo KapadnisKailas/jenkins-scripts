b9db59931e08be540fc32df47ea4a8e8d346a468
Kapadnis, Kailas <kapadnis.kailas@gmail.com>
2020-01-26 18:20:06 -0800

    added git node username & email

--- SpringBootDemoProjectViaDsl.groovy
+++ SpringBootDemoProjectViaDsl.groovy
@@ -1,7 +1,10 @@
 job('spring-boot-demo-project-via-dsl'){
   description('maven project job created via dsl')
   scm{
-    git('https://github.com/KapadnisKailas/spring-boot-demo-project.git', 'master')		
+    git('https://github.com/KapadnisKailas/spring-boot-demo-project.git', 'master'){ node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('KapadnisKailas')
+            node / gitConfigEmail('kapadnis.kailas@gmail.com')
+        }		
   }
   triggers{
     scm('H/45 * * * *')
