667f3d939ff782c04caccbc324969d062ec83b54
Kapadnis, Kailas <kapadnis.kailas@gmail.com>
2020-01-26 17:22:05 -0800

    renamed groovy file name as name should not contain the special characters

---
+++ SpringBootDemoProjectViaDsl.groovy
@@ -0,0 +1,15 @@
+job(“spring-boot-demo-project-via-dsl”){
+  description(“maven project job created via dsl”)
+  scm{
+    git('https://github.com/KapadnisKailas/spring-boot-demo-project.git', master)		
+  }
+  triggers{
+    scm('H/45 * * * *')
+  }
+  steps{
+    maven('clean package', 'pom.xml')
+  }
+  publishers{
+    archiveArtifacts '**/*.jar'	
+  } 		
+}
